00fa378b6b9dcecd02c6a69563490ae5c2ceaac6
senapahlevi <senapahlevi1@gmail.com>
2022-02-16 17:05:23 +0700

    create post api

--- sport/pages/api/learnapi/commento.js
+++ sport/pages/api/learnapi/commento.js
@@ -1,5 +1,15 @@
 import { comments } from "../../../learndataapi/comments";
 //you can access like these http://localhost:3000/api/learnapi/commento
 export default function handler(req, res) {
-  res.status(200).json(comments);
+  if (req.method === "GET") {
+    res.status(200).json(comments);
+  } else if (req.method === "POST") {
+    const posting = req.body.posting; //ini posting refer ke stringify yg ada posting
+    const newComment = {
+      id: Date.now(),
+      text: posting,
+    };
+    comments.push(newComment);
+    res.status(201).json(newComment);
+  }
 }
