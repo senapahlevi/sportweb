69f8318e8343e119524f1b49ef587304d260c1bc
senapahlevi <senapahlevi1@gmail.com>
2022-02-16 18:36:48 +0700

    fix learnApi variable

--- sport/component/learnAPI/komentar.js
+++ sport/component/learnAPI/komentar.js
@@ -1,25 +1,45 @@
 import React, { useState } from "react";
-
 const Komentar = () => {
   const [comments, setComments] = useState([]);
   const [posting, setPosting] = useState(""); //these for POST submit
   //these show or load data
   const fetchCommento = async () => {
-    const response = await fetch("/api/learnapi/commento");
-    const data = await response.json();
-    setComments(data);
+    try {
+      const response = await fetch("/api/learnapi");
+      const data = await response.json();
+      setComments(data);
+    } catch (e) {
+      console.error("ada errorr");
+    }
   };
   //post new comments/input data to array in api/learnApi/commento
   const submitCommento = async () => {
-    const response = await fetch("api/learnapi/commento", {
-      method: "POST",
-      body: JSON.stringify({ posting }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch("api/learnapi", {
+        method: "POST",
+        body: JSON.stringify({ posting }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.error("ada error submit");
+    }
+  };
+
+  const deleteCommento = async (commentId) => {
+    try {
+      const response = await fetch(`/api/learnapi/${commentId}`, {
+        method: "DELETE",
+      });
+      const data = await response.json(); //convert to json
+      console.log(data);
+      fetchCommento(); //show after deleted
+    } catch (e) {
+      console.error(e, "delete commento");
+    }
   };
   return (
     <div>
@@ -32,6 +52,7 @@ const Komentar = () => {
             <h1>
               {comment.id}
               {comment.text}
+              {/* <button onClick={() => deleteCommento(comment.id)}>Delete</button> */}
             </h1>
           </div>
         );
--- sport/pages/api/learnapi/[commentId].js
+++ sport/pages/api/learnapi/[commentId].js
@@ -3,6 +3,18 @@ import { comments } from "../../../learndataapi/comments";
 export default function handler(req, res) {
   //these to show only match id
   const { commentId } = req.query; //these extract
-  const posting = comments.find((comment) => comment.id === parseInt(commentId));
-  res.status(200).json(posting);
+  if (req.method === "GET") {
+    const posting = comments.find((comment) => comment.id === parseInt(commentId));
+    res.status(200).json(posting);
+  } else if (req.method === "DELETE") {
+    try {
+      const deleteposting = comments.find((comment) => comment.id === parseInt(commentId));
+      //these for find index and using slice to delete logic array
+      const index = comments.findIndex((comment) => comment.id === parseInt(commentId));
+      comments.splice(index, 1); //delete only 1
+      res.status(200).json(deleteposting);
+    } catch (e) {
+      console.error("error bagian delete");
+    }
+  }
 }
